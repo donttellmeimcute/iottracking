1116fa132a0ee9e5bd7da1cc37c014561523cf97
Stolas <24362190+donttellmeimcute@users.noreply.github.com>
2024-02-06 19:38:43 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -8,21 +8,17 @@ kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 
 while True:
     ret, frame = cap.read()
-    if ret == False: break
+    if not ret:
+        break
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Dibujamos un rectángulo en frame, para señalar el estado
-    # del área en análisis (movimiento detectado o no detectado)
     cv2.rectangle(frame,(0,0),(frame.shape[1],40),(0,0,0),-1)
     color = (0, 255, 0)
     texto_estado = "Estado: No se ha detectado movimiento"
 
-    # Especificamos los puntos extremos del área a analizar
     area_pts = np.array([[0, 0], [frame.shape[1], 0], [frame.shape[1], frame.shape[0]], [0, frame.shape[0]]])
     
-    # Con ayuda de una imagen auxiliar, determinamos el área
-    # sobre la cual actuará el detector de movimiento
     imAux = np.zeros(shape=(frame.shape[:2]), dtype=np.uint8)
     imAux = cv2.drawContours(imAux, [area_pts], -1, (255), -1)
     image_area = cv2.bitwise_and(gray, gray, mask=imAux)
@@ -31,9 +27,8 @@ while True:
     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
     fgmask = cv2.dilate(fgmask, None, iterations=2)
 
-    cnts = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+    cnts, _ = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
-    # Variables para almacenar la persona más cercana al centro
     min_distance = float('inf')
     closest_person = None
     
@@ -44,7 +39,6 @@ while True:
             center_y = y + h // 2
             distance = np.sqrt((frame.shape[1] // 2 - center_x) ** 2 + (frame.shape[0] // 2 - center_y) ** 2)
             
-            # Actualizar la persona más cercana al centro
             if distance < min_distance:
                 min_distance = distance
                 closest_person = cnt
@@ -55,8 +49,6 @@ while True:
         texto_estado = "Estado: Alerta Movimiento Detectado!"
         color = (0, 0, 255)    
 
-    # Visualizamos el alrededor del área que vamos a analizar
-    # y el estado de la detección de movimiento        
     cv2.drawContours(frame, [area_pts], -1, color, 2)
     cv2.putText(frame, texto_estado , (10, 30),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, color,2)
@@ -64,8 +56,7 @@ while True:
     cv2.imshow('fgmask', fgmask)
     cv2.imshow("frame", frame)
 
-    k = cv2.waitKey(70) & 0xFF
-    if k == 27:
+    if cv2.waitKey(70) == 27:
         break
 
 cap.release()
